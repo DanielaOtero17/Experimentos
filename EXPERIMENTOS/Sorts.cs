403d080e558c458b65051ec7df9fba9c75c629bc
DanielaOtero17 <jessot@hotmail.com>
2019-08-25 22:39:55 -0500

    Merge sort is already implemented.

--- EXPERIMENTOS/Sorts.cs
+++ EXPERIMENTOS/Sorts.cs
@@ -8,6 +8,66 @@ namespace EXPERIMENTOS
 {
     class Sorts
     {
+        //atributos para el merge sort.
+        private int[] array;
+        private int[] arrayMerge;
+        private int tamanio;
 
+        // constructor del merge.
+        public void sort(int[] Arr)
+        {
+            array = Arr;
+            tamanio = Arr.Length;
+            arrayMerge = new int[tamanio];
+            MergeSort(0, tamanio - 1);
+        }
+
+        //metodo que divide el arreglo en 2 partes
+        private void MergeSort(int pequenio, int grande)
+        {
+
+            if (pequenio < grande)
+            {
+                int mitad = pequenio + ((grande - pequenio) / 2);
+                MergeSort(pequenio, mitad);
+                MergeSort(mitad + 1, grande);
+                mergeParts(pequenio, mitad, grande);
+            }
+        }
+
+        // merge.
+        private void mergeParts(int pequenio, int mitad, int grande)
+        {
+            
+
+            for ( int a = pequenio; a <= grande; a++)
+            {
+                arrayMerge[a] = array[a];
+            }
+
+            int i = pequenio;
+            int j = mitad + 1;
+            int k = pequenio;
+            while (i <= mitad && j <= grande)
+            {
+                if (arrayMerge[i] <= arrayMerge[j])
+                {
+                    array[k] = arrayMerge[i];
+                    i++;
+                }
+                else
+                {
+                    array[k] = arrayMerge[j];
+                    j++;
+                }
+                k++;
+            }
+            while (i <= mitad)
+            {
+                array[k] = arrayMerge[i];
+                k++;
+                i++;
+            }
+        }
     }
 }
