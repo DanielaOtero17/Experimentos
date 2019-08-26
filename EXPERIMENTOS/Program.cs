02123a0effec918cab05410a413769125dd27b92
1144209785 <1144209785@SC203E07>
2019-08-26 15:14:57 -0500

    READ TXT

--- EXPERIMENTOS/Program.cs
+++ EXPERIMENTOS/Program.cs
@@ -18,5 +18,27 @@ namespace EXPERIMENTOS
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
+
+
+        public int[] readText (){
+            int counter = 0;
+            String line ;
+            String[] arrayAux;
+            int[] arrayInt ;
+
+            System.IO.StreamReader file = new System.IO.StreamReader(@"c:\test.txt");  
+
+            while(file.Peek() != 1){  
+               arrayAux = line.ReadLine().Split(",");
+               counter++;
+               int numero = Integer.parseInt(arrayAux[counter]) ;  
+               arrayInt[counter] = numero;
+              }  
+
+             file.Close();  
+
+            return arrayInt ;
+        }
+
     }
 }
